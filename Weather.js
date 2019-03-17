0a9e91f0d7091533d769c0d9b6031f42c1786820
Al Harvey <alharvey789@gmail.com>
2019-03-16 20:10:21 -0500

    :hammer: Connected to Dark SKy API

--- Weather.js
+++ Weather.js
@@ -1,20 +1,21 @@
 window.addEventListener('load', () => {
     var longitude;
     var latitude;
+    var tempratureDescription = document.querySelector('.temperature-description');
+    var tempratureDegree = document.querySelector('.temperature-degree');
+    var locationTimezone = document.querySelector('.location-timezone');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((position) => {
             longitude = position.coords.longitude;
             latitude = position.coords.latitude;
-
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
             
-            const darkSky = `${proxy}https://api.darksky.net/forecast/3315f1f22d1166c49217856a2a560482/${latitude},${longitude}`;
+            const darkSky = `https://api.darksky.net/forecast/3315f1f22d1166c49217856a2a560482/${latitude},${longitude}`;
 
             fetch(darkSky)
                 .then((response) => response.json())
                 .then((data) => {
-                    console.log(data);
+                    const { temperature, summary   } = data.currently;
                 });
         });
     }
