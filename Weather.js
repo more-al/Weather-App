d70d19d2087e296647a074ff0a4f6237c6f40195
Al Harvey <alharvey789@gmail.com>
2019-03-16 22:18:15 -0500

    Trying a new API without CORS restriction

--- Weather.js
+++ Weather.js
@@ -6,22 +6,23 @@ window.addEventListener('load', () => {
     var tempratureDescription = document.querySelector('.temperature-description');
     var tempratureDegree = document.querySelector('.temperature-degree');
     var locationTimezone = document.querySelector('.location-timezone');
-    
+
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((position) => {
             longitude = position.coords.longitude;
             latitude = position.coords.latitude;
 
-            const darkSky = `https://api.darksky.net/forecast/3315f1f22d1166c49217856a2a560482/${latitude},${longitude}`;
+            const darkSky = `https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a6a56da06c4a778a16e072cafcec86ff/${latitude},${longitude}`;
 
-            axios
-                .get(darkSky)
+            axios;
+            fetch(darkSky)
                 .then((response) => response.json())
                 .then((data) => {
-                    const { temperature, summary } = data.currently;
-                    // Set DOM Elemnts from the API
+                    console.log(data);
+                    const { temperature, summary } = data.curently;
 
+                    // Set DOM Elemnts from the API
                     tempratureDegree.textContent = temperature;
                     tempratureDescription.textContent = summary;
                     locationTimezone.textContent = data.timezone;
