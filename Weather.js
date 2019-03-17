e3821cfde6730a96b4392cecfec9f2a1dfb5eec9
Al Harvey <alharvey789@gmail.com>
2019-03-16 20:21:34 -0500

    :hammer: Connected to Dark SKy API

--- Weather.js
+++ Weather.js
@@ -1,21 +1,26 @@
+import axios from 'axios';
+import { get } from 'https';
+
 window.addEventListener('load', () => {
     var longitude;
     var latitude;
     var tempratureDescription = document.querySelector('.temperature-description');
     var tempratureDegree = document.querySelector('.temperature-degree');
     var locationTimezone = document.querySelector('.location-timezone');
+    
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((position) => {
             longitude = position.coords.longitude;
             latitude = position.coords.latitude;
-            
+
             const darkSky = `https://api.darksky.net/forecast/3315f1f22d1166c49217856a2a560482/${latitude},${longitude}`;
 
-            fetch(darkSky)
+            axios
+                .get(darkSky)
                 .then((response) => response.json())
                 .then((data) => {
-                    const { temperature, summary   } = data.currently;
+                    const { temperature, summary } = data.currently;
                     // Set DOM Elemnts from the API
 
                     tempratureDegree.textContent = temperature;
