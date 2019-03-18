cb32d30af94949a36d3f9c6d45f0827dcd236dfc
Al Harvey <alharvey789@gmail.com>
2019-03-18 07:46:27 -0500

    Deleted the load eventlistener

--- Weather.js
+++ Weather.js
@@ -1,31 +1,29 @@
-window.addEventListener('load', () => {
-    let longitude;
-    let latitude;
-    let tempratureDescription = document.querySelector('.temperature-description');
-    let tempratureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
+let longitude;
+let latitude;
+let tempratureDescription = document.querySelector('.temperature-description');
+let tempratureDegree = document.querySelector('.temperature-degree');
+let locationTimezone = document.querySelector('.location-timezone');
 
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition((position) => {
-            longitude = position.coords.longitude;
-            latitude = position.coords.latitude;
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition((position) => {
+        longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
 
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const darkSky = `${proxy}https://api.darksky.net/forecast/0352f8310ea90b32364e0064ecc11103/${latitude},${longitude}`;
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const darkSky = `${proxy}https://api.darksky.net/forecast/0352f8310ea90b32364e0064ecc11103/${latitude},${longitude}`;
 
 
-            fetch(darkSky)
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log(data);
-                    const { temperature, summary } = data.currently;
-                    // Set DOM Elemnts from the API
+        fetch(darkSky)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                const { temperature, summary } = data.currently;
+                // Set DOM Elemnts from the API
 
-                    tempratureDegree.textContent = temperature;
-                    tempratureDescription.textContent = summary;
-                    locationTimezone.textContent = data.timezone;
-                });
-        });
-    }
-});
+                tempratureDegree.textContent = temperature;
+                tempratureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+            });
+    });
+}
